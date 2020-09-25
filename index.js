fdbc2baf6d9ddc73137c2c95ae814b6a47a5dc3c
Orion <https://doesN0tComput3@github.com>
2020-09-25 09:32:51 -0700

    Add more commands

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const { prefix, token, fart_facts } = require('./config.json');
 
 client.once('ready', () => {
     console.log('i\'m back bitches');
+    client.user.setPresence({
+    activity: { name: 'BAKI BAKI' }
+    });
 });
 
 client.login(token);
@@ -19,5 +22,11 @@ client.on('message', message => {
         message.channel.send('oikawa shouldve came to shiratorizawa');
     } else if (message.content === 'fart fact') {
         message.channel.send(`Fart fact: ${fart_facts[Math.floor(Math.random() * fart_facts.length)]}`);
+    } else if (message.content === 'hey tendou' || message.content === 'hi tendou') {
+        message.channel.send(`hey ${message.author} :smirk:`)
+    } else if (message.content === 'tendou where can i buy drugs') {
+        message.channel.send(`don't buy drugs ${message.author}`)
+    } else if (message.content === 'tendou are you a doctor') {
+        message.channel.send('no, but you should go to one')
     }
 });
