349287b43933093a8ca0cf14b5a25fe7937f1c57
doesN0tComput3 <48813805+doesN0tComput3@users.noreply.github.com>
2020-09-25 21:18:26 -0700

    Remove command handler and improve ping command

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const Discord = require('discord.js');
-const fs = require('fs');
 
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
 
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 const {
     prefix,
     token,
@@ -20,21 +18,10 @@ client.once('ready', () => {
 
 client.login(token);
 
-for (const file of commandFiles) {
-    const command = require(`./commands/${file}`);
-    client.comands.set(command.name, command);
-
-    if (!client.commands.has(command)) return;
-    try {
-        client.commands.get(command).execute(message, args);
-    } catch (error) {
-        console.error(error)
-        message.channel.send(`Sorry ${message.author}, something went wrong :(`);
-    }
-}
-
 client.on('message', message => {
-    if (message.content === 'i love you tendou') {
+    if (message.content === 'ping') {
+        message.channel.send(`pong bitch\nLatency is ${m.createdTimestamp - message.createdTimestamp}ms\nAPI latency is ${Math.round(client.ping)}ms`);
+    } else if (message.content === 'i love you tendou') {
         message.channel.send(`i love you too ${message.author} :kissing_heart:`);
     } else if (message.content === 'you\'re so amazing tendou') {
         message.channel.send(`you look even better ${message.author}`);
