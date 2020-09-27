55aa518e22680dbf96fb40d85e77c00815d67c9e
doesN0tComput3 <48813805+doesN0tComput3@users.noreply.github.com>
2020-09-27 09:38:35 -0700

    Improve reply command and clean up bot

--- index.js
+++ index.js
@@ -2,12 +2,7 @@
 // Get variables
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const {
-    prefix,
-    token,
-    fart_facts
-} = require('./config.json');
-const mainChannel = client.channels.cache.get('749084221024239717');
+const config = require('./config.json');
 
 // These commands run on startup
 client.once('ready', () => {
@@ -15,37 +10,58 @@ client.once('ready', () => {
     console.log('i\'m back lol');
 
     // Set activity to my song
-    client.user.setActivity('BAKI BAKI', { type: 'LISTENING'})
-
-    // Send message in server to let others know I'm back (add)
+    client.user.setActivity('BAKI BAKI', { type: 'LISTENING' });
 });
 
 // Login to bot (duh)
-client.login(token);
+client.login(config.token);
 
 // THE COMMANDS!!!
 client.on('message', async message => {
+    const responses = {
+        // Love you tendou
+        'i love you tendou': `i love you too ${message.author} :kissing_heart:`,
+        'I love you tendou': `i love you too ${message.author} :kissing_heart:`,
+        'I love you Tendou': `i love you too ${message.author} :kissing_heart:`,
+        // Amazing tendou
+        'you\'re so amazing tendou': `you look even better ${message.author}`,
+        'You\'re so amazing tendou': `you look even better ${message.author}`,
+        'You\'re so amazing Tendou': `you look even better ${message.author}`,
+        // Thoughts on Oikawa?
+        'what are ur thoughts on oikawa': 'oikawa shouldve came to shiratorizawa',
+        'What are your thoughts on oikawa': 'oikawa shouldve came to shiratorizawa',
+        'What are your thoughts on Oikawa': 'oikawa shouldve came to shiratorizawa',
+        // Heyyyy tendou
+        'hey tendou': `hey ${message.author} :smirk:`,
+        'Hey Tendou': `hey ${message.author} :smirk:`,
+        'Hey tendou': `hey ${message.author} :smirk:`,
+        'hey Tendou': `hey ${message.author} :smirk:`,
+        'hi tendou': `hey ${message.author} :smirk:`,
+        'Hi Tendou': `hey ${message.author} :smirk:`,
+        'Hi tendou': `hey ${message.author} :smirk:`,
+        'hi Tendou': `hey ${message.author} :smirk:`,
+        // I need drugs lol
+        'tendou where can i buy drugs': `don't buy drugs ${message.author}`,
+        'Tendou where can I buy drugs': `don't buy drugs ${message.author}`,
+        'where can i buy drugs tendou': `don't buy drugs ${message.author}`,
+        // Doctor tendou?
+        'tendou are you a doctor': 'no, but you should go to one',
+        'are you a doctor tendou': 'no, but you should go to one',
+        // Baki baki something idk
+        'baki baki ni ore': 'nani wo? kokoro wo da yo',
+        // Do you love me
+        'tendou do you love me': `of course my love ${message.author} :pleading_face:`,
+        'Tendou do you love me': `of course my love ${message.author} :pleading_face:`,
+        'do you love me tendou': `of course my love ${message.author} :pleading_face:`,
+        // Fart facts!
+        'fart fact': `Fart fact: ${config.fart_facts[Math.floor(Math.random() * config.fart_facts.length)]}`
+    };
     // Ping command
-    if (message.content === `${prefix}ping`) {
+    if (message.content === `${config.prefix}ping`) {
         const m = await message.channel.send('pong bitch');
         m.edit(`pong bitch\nLatency is ${m.createdTimestamp - message.createdTimestamp}ms`);
-    // Regular message commands
-    } else if (message.content === 'i love you tendou') {
-        message.channel.send(`i love you too ${message.author} :kissing_heart:`);
-    } else if (message.content === 'you\'re so amazing tendou') {
-        message.channel.send(`you look even better ${message.author}`);
-    } else if (message.content === 'what are ur thoughts on oikawa') {
-        message.channel.send('oikawa shouldve came to shiratorizawa');
-    } else if (message.content === 'hey tendou' || message.content === 'hi tendou') {
-        message.channel.send(`hey ${message.author} :smirk:`);
-    } else if (message.content === 'tendou where can i buy drugs') {
-        message.channel.send(`don't buy drugs ${message.author}`);
-    } else if (message.content === 'tendou are you a doctor') {
-        message.channel.send('no, but you should go to one');
-    } else if (message.content === 'baki baki ni ore') {
-        message.channel.send('nani wo? kokoro wo da yo');
-    // Fart facts!
-    } else if (message.content === 'fart fact') {
-        message.channel.send(`Fart fact: ${fart_facts[Math.floor(Math.random() * fart_facts.length)]}`);
+    // Reply (inputs and outputs)
+    } else if (responses[message.content]) {
+        message.channel.send(responses[message.content]);
     }
 });
