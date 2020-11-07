cd978f7918b7a6a63799e54405f5fb5ed82b04bc
doesN0tComput3 <48813805+doesN0tComput3@users.noreply.github.com>
2020-11-07 14:51:06 -0800

    Create poll command

---
+++ commands/poll.js
@@ -0,0 +1,35 @@
+const Discord = require('discord.js');
+
+module.exports = {
+    name: 'poll',
+    description: 'Sends a message to a person',
+    category: 'fun',
+    usage: '[question]',
+    args: true,
+    execute(message, args) {
+        const question = args.join(' ');
+
+        const pollEmbed = new Discord.MessageEmbed()
+            .setColor('#00FFFF')
+            .setTitle('Poll')
+            .addField('**Question**', question, true)
+            .addField('**Poll Started By**', message.author)
+            .setThumbnail(message.author.avatarURL)
+            .setFooter('FartBot2000', message.client.user.avatarURL());
+
+        const channel = message.client.channels.cache.find(channel => channel.id === '749084221024239717');
+        channel.send(pollEmbed).then(messageReaction => {
+            messageReaction.react('👍');
+            messageReaction.react('👎');
+        });
+
+        const embed = new Discord.MessageEmbed()
+            .setColor('39ff14')
+            .setTitle('Poll created!')
+            .setDescription('Your poll was created.')
+            .addField('**Question**', question, true)
+            .setFooter('FartBot2000', message.client.user.avatarURL());
+
+        message.channel.send(embed);
+    }
+};
