887777f66cdf0d934985c2653d1a3981deedbd5c
doesN0tComput3 <48813805+doesN0tComput3@users.noreply.github.com>
2020-11-09 10:14:36 -0800

    repost if jesus ballin

---
+++ commands/jesusballin.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'jesusballin',
+    description: 'repost if jesus ballin',
+    category: 'fun',
+    execute(message) {
+        message.channel.send('https://cdn.discordapp.com/attachments/749084221024239717/775417382989594635/image0.gif');
+    }
+};
