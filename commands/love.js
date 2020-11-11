0dba5b4b20041ee7e3073738df14e40f8c4c87cf
doesN0tComput3 <48813805+doesN0tComput3@users.noreply.github.com>
2020-11-10 21:21:13 -0800

    Create love command

---
+++ commands/love.js
@@ -0,0 +1,27 @@
+const Discord = require('discord.js');
+
+module.exports = {
+    name: 'love',
+    aliases: ['loveaffinity'],
+    description: 'How much does this person love you?\nYou don\'t have to @ someone, but you can if you want!',
+    category: 'fun',
+    usage: 'person',
+    args: true,
+    execute(message, args) {
+        const person = args.join(' ');
+
+        const love = Math.random() * 100;
+        const loveIndex = Math.floor(love / 10);
+        const loveLevel = '💖'.repeat(loveIndex) + '💔'.repeat(10 - loveIndex);
+
+        const embed = new Discord.MessageEmbed()
+            .setColor('RANDOM')
+            .setTitle(`How much does ${person} love ${message.author.username}?`)
+            .setDescription(`**${person}** loves **${message.author.username}** this much:`)
+            .addField('Percentage', `**${Math.floor(love)}%\n${loveLevel}**`)
+            .setThumbnail(message.author.avatarURL())
+            .setFooter('FartBot2000 | !help', message.client.user.avatarURL());
+
+        message.channel.send(embed);
+    }
+};
