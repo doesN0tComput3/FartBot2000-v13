0d9546c9b1f6efcfb0bcb1c2af9b4e19eb8a1cb4
doesN0tComput3 <48813805+doesN0tComput3@users.noreply.github.com>
2020-11-11 20:33:24 -0800

    Create economy system

---
+++ commands/register.js
@@ -0,0 +1,29 @@
+const Discord = require('discord.js');
+const fs = require('fs');
+
+module.exports = {
+    name: 'register',
+    aliases: ['start'],
+    description: 'Registers you into the economy!',
+    category: 'economy',
+    execute(message) {
+        const UserJSON = JSON.parse(fs.readFileSync('../DB/users.json'));
+
+        UserJSON[message.author.id] = {
+            bal: 50,
+            lastclaim: 0
+        };
+
+        fs.writeFileSync('../DB/users.json', JSON.stringify(UserJSON));
+
+        const embed = new Discord.MessageEmbed()
+            .setColor('#39ff14')
+            .setTitle('**Success!**')
+            .setDescription('Successfully registered you into the economy.\nAs a reward, you start of with **50 FartCoins!**')
+            .addField('Current Balance', `**${UserJSON[message.author.id].bal} FartCoins**`)
+            .setThumbnail(message.author.avatarURL())
+            .setFooter('FartBot2000 | !help', message.client.user.avatarURL());
+
+        message.channel.send(embed);
+    }
+};
