030650bedf25a4d7355dfb1a0ee33755fb56eaf3
doesN0tComput3 <48813805+doesN0tComput3@users.noreply.github.com>
2020-11-14 09:07:53 -0800

    Create XP system

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const Discord = require('discord.js');
 const { prefix, token } = require('./config.json');
+const xp = require('./xp.json');
 
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
@@ -23,7 +24,39 @@ client.once('ready', () => {
 });
 
 client.on('message', message => {
-	if (!message.content.startsWith(prefix) || message.author.bot) return;
+	if (message.author.bot) return;
+
+	const xpAdd = Math.floor(Math.random() * 7) + 8;
+	console.log(xpAdd);
+
+	if (!xp[message.author.id]) {
+		xp[message.author.id] = {
+			xp: 0,
+			level: 0
+		};
+	}
+
+	const currentXp = xp[message.author.id].xp;
+	const currentLevel = xp[message.author.id].level;
+	const nextLevel = xp[message.author.id].level * 150;
+	xp[message.author.id].xp = currentXp + xpAdd;
+
+	if (nextLevel <= xp[message.author.id].xp) {
+		xp[message.author.id].level = currentLevel + 1;
+		const levelUpEmbed = new Discord.MessageEmbed()
+			.setColor('#39ff14')
+			.setTitle('**LEVEL UP!**')
+			.setDescription(`You just leveled up to **level ${currentLevel + 1}!**`)
+			.setThumbnail(`${message.author.avatarURL()}`)
+			.setFooter('FartBot2000 | !help', message.client.user.avatarURL());
+		message.channel.send(levelUpEmbed);
+	}
+
+	fs.writeFile('./xp.json', JSON.stringify(xp), (err) => {
+		if (err) console.log(err);
+	});
+
+	if (!message.content.startsWith(prefix)) return;
 
 	const args = message.content.slice(prefix.length).trim().split(/ +/);
 	const commandName = args.shift().toLowerCase();
