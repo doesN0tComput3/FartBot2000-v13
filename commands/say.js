7e4a19de1bf9ab71ec596eddb0de6ee2d7c51a2a
doesN0tComput3 <48813805+doesN0tComput3@users.noreply.github.com>
2020-11-14 09:10:18 -0800

    Make changes to say command

--- commands/say.js
+++ commands/say.js
@@ -7,20 +7,13 @@ module.exports = {
     usage: '[message]',
     args: true,
     execute(message, args) {
-        if (!message.channel.type === 'dm') message.delete();
-        const sayText = args.join(' ');
-        const channel = message.client.channels.cache.find(channel => channel.id === '749084221024239717');
-
-        channel.send(sayText);
-
-        const embed = new Discord.MessageEmbed()
-            .setColor('#39ff14')
-            .setTitle(`Message sent to #${channel.name}!`)
-            .setDescription('Your message was successfully sent.')
-            .addField('**Message**', sayText, true)
-            .setThumbnail(message.author.avatarURL())
-            .setFooter('FartBot2000 | !help', message.client.user.avatarURL());
-
-        message.author.send(embed);
+        if (!message.channel.type === 'dm') {
+            message.delete();
+            const sayText = args.join(' ');
+            message.channel.send(sayText);
+        } else {
+            const sayText = args.join(' ');
+            message.channel.send(sayText)
+        }
     }
 };
