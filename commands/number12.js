5f43de2fbf34762cf7b1a7f8afd011dafd7736e1
Orion <orionlani777@gmail.com>
2021-01-04 20:02:50 -0800

    Create more vc commands

---
+++ commands/number12.js
@@ -0,0 +1,36 @@
+const Discord = require('discord.js');
+const config = require('../config.json');
+const ytdl = require('ytdl-core');
+
+module.exports = {
+    name: 'number12',
+    description: 'cok',
+    category: 'voice',
+    execute(message) {
+        // Define vc
+        const vc = message.member.voice.channel;
+
+        if (vc) {
+            // Join vc
+            const connection = vc.join()
+                .then(connection => {
+                    // Self deafen so we don't recieve audio
+                    connection.voice.setDeaf(true);
+                    // Get our video
+                    const stream = ytdl('https://www.youtube.com/watch?v=hwk5XZh2syU', { filter: 'audioonly' });
+                    // Play
+                    const dispatcher = connection.play(stream);
+                    // Leave once it's over
+                    dispatcher.on('finish', () => {
+                        vc.leave();
+                    });
+                })
+                .catch();
+        } else {
+            // They aren't in vc so I'm not wasting my time
+            return message.reply('bro you\'re not in vc i can\'t')
+        };
+
+
+    }
+}
---
+++ commands/stop.js
@@ -0,0 +1,18 @@
+const Discord = require('discord.js');
+const config = require('../config.json');
+const ytdl = require('ytdl-core');
+
+module.exports = {
+    name: 'stop',
+    description: 'Leaves the vc',
+    aliases: ['die', 'leave'],
+    category: 'voice',
+    execute(message) {
+        const vc = message.member.voice.channel;
+
+        if (vc) {
+            // Join vc
+            const connection = vc.leave()
+        } else return;
+    }
+}
