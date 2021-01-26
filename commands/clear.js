1a08a368e7cbeec51b7906ac9f667da22f84b093
Orion <48813805+doesN0tComput3@users.noreply.github.com>
2021-01-26 09:37:35 -0800

    Merge userclear and clear commands

--- commands/clear.js
+++ commands/clear.js
@@ -1,26 +1,56 @@
 module.exports = {
 	name: 'clear',
 	aliases: ['delete'],
-	description: 'Deletes messages',
+	description: 'Deletes messages, or from a specific person',
 	category: 'mod',
-	usage: '[number < 100]',
+	usage: '[number < 100], [@member] [number < 100]',
 	guildOnly: true,
 	permissions: 'MANAGE_MESSAGES',
 	args: true,
 	async execute(message, args) {
-		// Returns
-		// Return if amount isn't a number
-		if (isNaN(args[0])) return message.channel.send('❌ thats not a number idiot');
+		if (message.mentions.users.first()) {
+			// Returns
+			// Return if amount isn't a number
+			if (isNaN(args[1])) return message.channel.send('❌ thats not a number idiot');
 
-		// Return if number is greater than 100
-		if(args[0] > 100) return message.channel.send(`❌ ${message.author} you can't delete more than 100 messages sorry`);
+			// Return if number is greater than 100
+			if(args[1] > 100) return message.channel.send(`❌ ${message.author} you can't delete more than 100 messages sorry`);
 
-		// Return if number is less than 1
-		if(args[0] < 1) return message.channel.send(`❌ ${message.author} you have to delete at least one!`);
+			// Return if number is less than 1
+			if(args[1] < 1) return message.channel.send(`❌ ${message.author} you have to delete at least one!`);
 
-		// Delete messages
-		message.channel.bulkDelete(args[0]).then(() => {
-			message.channel.send(`✅ Successfully deleted ${args[0]} message(s)!`);
-		});
+			// Define target
+			const target = message.mentions.users.first();
+			if (!target) return message.channel.send('❌ couldn\'t find that person');
+
+			// Delete messages
+			message.channel.messages.fetch({
+				limit: 100
+			}).then((messages) => {
+				if (target) {
+					const filterBy = target ? target.id : message.client.target.id;
+
+					messages = messages.filter(m => m.author.id === filterBy).array().slice(0, args[1]);
+				}
+				message.channel.bulkDelete(messages).then(() => {
+					message.channel.send(`✅ Successfully deleted ${args[1]} message(s) from ${target}!`);
+				});
+			});
+		} else {
+			// Returns
+			// Return if amount isn't a number
+			if (isNaN(args[0])) return message.channel.send('❌ thats not a number idiot');
+
+			// Return if number is greater than 100
+			if(args[0] > 100) return message.channel.send(`❌ ${message.author} you can't delete more than 100 messages sorry`);
+
+			// Return if number is less than 1
+			if(args[0] < 1) return message.channel.send(`❌ ${message.author} you have to delete at least one!`);
+
+			// Delete messages
+			message.channel.bulkDelete(args[0]).then(() => {
+				message.channel.send(`✅ Successfully deleted ${args[0]} message(s)!`);
+			});
+		}
 	}
 };
